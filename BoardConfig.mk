60adeac7199c870f60c206454cce97f30dc0ea0c
mblaster <holger.gilbergs@gmail.com>
2011-11-14 21:41:09 +0100

    Set TARGET_USES_GL_VENDOR_EXTENSIONS

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -60,6 +60,7 @@ TARGET_OTA_ASSERT_DEVICE := folio100,betelgeuse
 TARGET_LIBAGL_USE_GRALLOC_COPYBITS := true
 BOARD_NO_RGBX_8888 := true
 BOARD_EGL_CFG := device/toshiba/folio100/egl.cfg
+TARGET_USES_GL_VENDOR_EXTENSIONS := true
 
 # Enables Old Sensor Compatibility Seems To Cause CPU Lockup, New kernel may be required
 TARGET_USES_OLD_LIBSENSORS_HAL:=true
